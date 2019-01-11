2f64590d35effdc912c41d11ceb63284f20dc034
Amanda Hewes <amanda.hewes@uconn.edu>
2019-01-11 16:19:17 -0500

    Pushing SWC to git

--- SoftwareCarpentry.R
+++ SoftwareCarpentry.R
@@ -129,4 +129,184 @@ Afghanistan
 Afghanistan$newcol<-Afghanistan$pop*Afghanistan$gdpPercap #creates new column in the Afghanistan data frame by multiplying the pop column by the gdp column (MUST write Afghanistan$ before each column so it knows which data frame youre pulling from)
 Afghanistan
 write.csv(Afghanistan, "data/afghan_data.csv") #writes the afghanistan datframe into the data file as a csv under the name afghan_data
-              
+
+#practice at home
+Algeria <- gapminder[25:36, 1:6]
+Algeria$newcol <- Algeria$pop*Algeria$gdpPercap
+Algeria$newcol
+
+Algeria
+write.csv (Algeria, "data/algeria_data.csv")
+
+#START SWC DAY 2
+install.packages("tidyr")
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+ggplot(data = gapminder, aes(x = gdpPercap, y = lifeExp)) + geom_point()
+install.packages("knitr")
+library(knitr)
+
+#Practice with scatter and line plots
+plot (gapminder$gdpPercap, gapminder$lifeExp)
+ggplot(data = gapminder, aes(x = gdpPercap, y = lifeExp, color=continent)) + geom_point()
+ggplot(data = gapminder, aes(x = gdpPercap, y = lifeExp, by=country, color=continent)) + geom_line()
+ggplot(data=gapminder, aes(x=year, y=lifeExp, color=continent)) + geom_point()
+ggplot(data = gapminder, aes(x = gdpPercap, y = lifeExp, by=country)) + geom_line(aes(color=continent))+geom_point(col="blue") #colors lines by continent according to gapminder dataset and colors points blue
+ggplot(data = gapminder, aes(x = gdpPercap, y = lifeExp, color=continent)) + geom_point(alpha = 0.5) + scale_x_log10() #alpha changes transparency of points, scale changes the x axis to a log scale
+ggplot(data = gapminder, aes(x = gdpPercap, y = lifeExp, color=continent)) + geom_point(alpha = 0.5) + scale_x_log10() +geom_smooth(method=lm) #geom_smooth(method_lm) adds trendlines with 95% CI to each continent cluster
+ggplot(data = gapminder, aes(x = gdpPercap, y = lifeExp, color=continent)) + geom_point(alpha = 0.5) + scale_x_log10() +geom_smooth(method=lm, size =1.5) #size changes the size of whatever the command is, e.g. points, lines
+
+#Socrative practice question
+ggplot(data = gapminder, aes(x = gdpPercap, y = lifeExp, color=continent)) + geom_point (size =0.5) + scale_x_log10() + geom_smooth(method="lm")
+
+#More practice
+ggplot(data = gapminder, aes(x = gdpPercap, y = lifeExp, color=continent)) + geom_point (aes(shape=continent), alpha=0.5, show.legend=FALSE) + scale_x_log10() + geom_smooth(method="lm", show.legend=FALSE) #show.lengend=FALSE takes away legend
+ggplot(data = gapminder, aes(x = gdpPercap, y = lifeExp, color=continent)) + 
+  geom_point(size=2, aes(shape=continent)) + 
+  scale_x_log10() +
+  geom_smooth(method=lm) +
+  scale_y_continuous(limits=c(0,100), breaks=seq(0,100, by=10)) #changes the y axis fomr 0 to 100 with breaks every 10
+ggplot(data = gapminder, aes(x = gdpPercap, y = lifeExp, color=continent)) + 
+  geom_point(size=2, aes(shape=continent)) + 
+  scale_x_log10() +
+  geom_smooth(method=lm) +
+  scale_y_continuous(limits=c(0,100), breaks=seq(0,100, by=10)) + 
+  theme_bw() #put the black square around the plot and took out the background shading
+ggplot(data = gapminder, aes(x = gdpPercap, y = lifeExp, color=continent)) + 
+  geom_point(size=2, aes(shape=continent)) + 
+  scale_x_log10() +
+  geom_smooth(method=lm) +
+  scale_y_continuous(limits=c(0,100), breaks=seq(0,100, by=10)) + 
+  theme_bw()+
+  ggtitle("Effects of per-capita GDP on life expectancy") #adds title
+ggplot(data = gapminder, aes(x = gdpPercap, y = lifeExp, color=continent)) + 
+  geom_point(alpha=0.5, size=2, aes(shape=continent)) + 
+  scale_x_log10() +
+  geom_smooth(method=lm) +
+  scale_y_continuous(limits=c(0,100), breaks=seq(0,100, by=10)) + 
+  theme_bw()+
+  ggtitle("Effects of per-capita GDP on life expectancy")+
+  xlab("GDP per capita ($)")+
+  ylab("Life Expectancy (years)") #adds x and y labels with units in ()
+ggsave(file="life_expectancy.pdf") #write this line IMMEDIATELY following the code for the graph and it will save to your project directory as whatever extension you put
+ggsave(file="life_expectancy.png", width=20, height=20, units="cm") #can give specific dimensions you want the image to save as
+
+#Making a boxplot
+ggplot(data = gapminder, aes(x = continent, y = lifeExp))+
+  geom_boxplot()+
+  geom_jitter(alpha=0.5, color="tomato") #geom_jitter give distribution of points, while geom_point gives a stright line
+
+#Making a violin plot
+ggplot(data = gapminder, aes(x = continent, y = lifeExp, color=continent))+
+  geom_violin()
+
+#Saving a plot as an object
+plot <- ggplot(data = gapminder, aes(x = continent, y = lifeExp))+
+  geom_boxplot()
+plot
+plot + geom_point(alpha=0.5, color="tomato") #If you save a plot as an object, then you can add more things to it without having to reenter the code for the actual graph
+plot+geom_jitter(alpha=0.5, color="tomato")
+
+#Basic programming in R - Letting R make choices 
+number <- 37
+if (number >100) {
+  print("greater than 100")
+} else {
+  print("less than or equal to 100")
+}
+#Other operators
+#Less than <
+#Equal to ==
+#Not equal to !=
+
+number <- -3
+if(number >0) {
+  print(1)
+} else if (number<0) {
+  print(-1)
+} else {
+  print(0)
+}
+
+number1
+number2
+if(number1 >=0 & number2>=0){
+  print("both numbers are positive")
+} else {
+  print("at least one number was negative")
+}
+number1 <- -15
+if(number1 >=0 & number2>=0){
+  print("both numbers are positive")
+} else {
+  print("at least one number was negative")
+}
+
+#Loops - allow you to look at a lot of data by automating the process
+numbers <- 1:10
+for(number in numbers) {
+  print(number)
+}
+for(i in 1:10) {
+  print(i)
+}
+print(i) #i is the incriment variable, can be used to tell you what the last piece in the loop was
+
+letter <- "z"
+print(letter)
+for(letter in c("a", "b", "c")) {
+  print(letter)
+}
+print(letter)
+
+numbers <- c(4, 8, 15, 16, 23, 42)
+numbers
+sum <- 0
+for (number in numbers) {
+  print(sum <- sum+number)
+} 
+sum
+
+sum<-0
+for(i in c(4,8,15,16,23,42)) {
+  print(sum<-sum+i)
+}
+sum
+
+numbers<-(1:10000)
+running_sum<-0
+for(number in numbers) {
+  running_sum<-running_sum+number
+}
+print(running_sum)
+
+#Functions - to store code you will want to use again
+fahr_to_kelvin<-function(temp) {
+  kelvin<-((temp-32)*(5/9))+273.15
+  return(kelvin)
+}
+fahr_to_kelvin(32)
+fahr_to_kelvin(212)
+
+kelvin_to_celsius<-function(temp) {
+  celsius<-temp-273.15
+  return(celsius)
+}
+kelvin_to_celsius(0)
+kelvin_to_celsius(545)
+
+fahr_to_kelvin <- function(temp) {
+  temp <- ((temp - 32) * (5 / 9)) + 273.15
+  return(temp) #The "temp" inside the function acts independently from the "temp" set as 73
+} 
+temp <- 73
+kelvin_temp <- fahr_to_kelvin(temp)
+print(temp)
+
+celsius_to_fahr<-function(temp) {
+  fahr<-(temp*9/5)+32
+  return(fahr)
+}
+celsius_to_fahr(0)
+celsius_to_fahr(100)
